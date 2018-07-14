6b03cb51aed125ac6cb156e944a3548039ae190c
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-07-14 13:20:21 +0200

    Scripts anteriores al proyecto git

---
+++ constants.py
@@ -0,0 +1,5 @@
+# TODO: Check more GPIO ports in Raspberry Pi 3 diagram
+GPIO_PORTS = {7, 11, 12, 13, 15, 16, 22, 29, 31, 32, 33, 35, 36, 37, 38, 40}
+GROUND_PORTS = {6, 9, 14, 20, 25, 30, 34, 39}
+VOLT_3_PORTS = {1, 17}
+VOLT_5_PORTS = {2, 4}
---
+++ engage_light.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#import RPi.GPIO as GPIO
+import argparse
+import constants
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", type=int, help="Numero de puerto del dispositivo",)
+
+args = parser.parse_args()
+port = args.port
+
+if port:
+    if port in constants.GPIO_PORTS:
+        print("Activación de puerto GPIO ", port)
+        # Activate port
+        #GPIO.setmode(GPIO.BCM)
+
+        #GPIO.setup(port, GPIO.OUT)
+        #GPIO.output(port, True)
+    else:
+        print("Puerto no válido, usa uno de los siguientes: ", constants.GPIO_PORTS)
+else:
+    print("¡Debes poner un puerto GPIO!")
